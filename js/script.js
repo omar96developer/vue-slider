f63d2a9af195da7804fa099b8a6a81ae91c13ad5
omar96developer <omakino@hotmail.it>
2021-11-01 22:35:32 +0100

    0.3.1

--- js/script.js
+++ js/script.js
@@ -4,10 +4,72 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            imagesList: [
+                {
+                    url: 'img/01.jpg',
+                    titolo: 'Svezia',
+                    descrizione: 'Lorem ipsum',
+                },
+                {
+                    url: 'img/02.jpg',
+                    titolo: 'Svizzera',
+                    descrizione: 'Lorem ipsum',
+                },
+                {
+                    url: 'img/03.jpg',
+                    titolo: 'Gran Bretagna',
+                    descrizione: 'Lorem ipsum',
+                },
+                {
+                    url: 'img/04.jpg',
+                    titolo: 'Germania',
+                    descrizione: 'Lorem ipsum',
+                },
+                {
+                    url: 'img/05.jpg',
+                    titolo: 'Paradise',
+                    descrizione: 'Lorem ipsum',
+                }
+            ]
         },
         methods: {
-            
+
         }
     }
 );
+
+/* 
+
+
+const items = [
+    {
+        url: 'img/01.jpg',
+        titolo: 'Svezia',
+        descrizione: 'Lorem ipsum',
+    },
+    {
+        url: 'img/02.jpg',
+        titolo: 'Svizzera',
+        descrizione: 'Lorem ipsum',
+    },
+    {
+        url: 'img/03.jpg',
+        titolo: 'Gran Bretagna',
+        descrizione: 'Lorem ipsum',
+    },
+    {
+        url: 'img/04.jpg',
+        titolo: 'Germania',
+        descrizione: 'Lorem ipsum',
+    },
+    {
+        url: 'img/05.jpg',
+        titolo: 'Paradise',
+        descrizione: 'Lorem ipsum',
+    }
+]
+
+
+
+
+*/
